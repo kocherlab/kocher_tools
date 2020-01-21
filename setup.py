41c8c09abe4279c25140228323c9dd5ea89ad3a9
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2020-01-21 18:41:02 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+import io
+import kocher_tools
+from setuptools import setup
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+requirements = ['pyyaml',
+                'pandas',
+                'Biopython',
+                'networkx']
+
+tool_scripts = ['kocher_tools/barcode_pipeline.py',
+                'kocher_tools/barcode_filter.py',
+                'kocher_tools/create_database.py',
+                'kocher_tools/retrieve_samples.py',
+                'kocher_tools/upload_samples.py']
+
+setup(name=kocher_tools.__title__,
+      version=kocher_tools.__version__,
+      project_urls={"Documentation": "https://ppp.readthedocs.io",
+                    "Code": "https://github.com/jaredgk/PPP",
+                    "Issue tracker": "https://github.com/jaredgk/PPP/issues"},
+      license=kocher_tools.__license__,
+      url=kocher_tools.__url__,
+      author=kocher_tools.__author__,
+      author_email=kocher_tools.__email__,
+      maintainer="Andrew Webb",
+      maintainer_email="19213578+aewebb80@users.noreply.github.com",
+      description=kocher_tools.__summary__,
+      long_description=readme,
+      include_package_data=True,
+      packages=['kocher_tools'],
+      install_requires=requirements,
+      scripts=tool_scripts,
+      python_requires=">=3.7")
+
+#packages=setuptools.find_packages() to automate package finding?
